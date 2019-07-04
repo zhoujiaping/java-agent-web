d95bf31455198e6eda5e7b1b4ecad92e5ed01c85
zhoujiaping <421211679@qq.com>
2019-07-04 20:33:14 +0800

    1

--- src/main/java/org/sirenia/TomcatStarter.java
+++ src/main/java/org/sirenia/TomcatStarter.java
@@ -8,32 +8,41 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TomcatStarter {
+	static{
+		System.setProperty("logback.configurationFile", "e:/logback.xml");
+	}
+	private static Logger logger = LoggerFactory.getLogger(TomcatStarter.class);
+
 	public static void main(String[] args) throws LifecycleException {
 		String webappDirLocation = "src/main/webapp/";
-        Tomcat tomcat = new Tomcat();
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8081 if it isn't there.
-        String webPort = System.getenv("PORT");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = "8081";
-        }
-        
-        tomcat.setPort(Integer.valueOf(webPort));
+		Tomcat tomcat = new Tomcat();
+		// The port that we should run on can be set into an environment
+		// variable
+		// Look for that variable and default to 8081 if it isn't there.
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8081";
+		}
 
-        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+		tomcat.setPort(Integer.valueOf(webPort));
 
-        // Declare an alternative location for your "WEB-INF/classes" dir
-        // Servlet 3.0 annotation will work
-        File additionWebInfClasses = new File("target/classes");
-        WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
-                additionWebInfClasses.getAbsolutePath(), "/"));
-        ctx.setResources(resources);
+		StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+		System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
-        tomcat.start();
-        tomcat.getServer().await();
+		// Declare an alternative location for your "WEB-INF/classes" dir
+		// Servlet 3.0 annotation will work
+		File additionWebInfClasses = new File("target/classes");
+		WebResourceRoot resources = new StandardRoot(ctx);
+		resources.addPreResources(
+				new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+		ctx.setResources(resources);
+
+		tomcat.start();
+		logger.info("tomcat started at port：{}", webPort);
+		tomcat.getServer().await();
 	}
 }
