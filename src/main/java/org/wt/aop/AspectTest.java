2fabb7c33e75aece348e56d35632f03b4a1b9cce
zhoujiaping <421211679@qq.com>
2019-08-08 14:54:27 +0800

    1

---
+++ src/main/java/org/wt/aop/AspectTest.java
@@ -0,0 +1,27 @@
+package org.wt.aop;
+
+import javax.annotation.PostConstruct;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AspectTest {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@PostConstruct
+	public void init(){
+		logger.info(this.getClass()+" init");
+	}
+
+	@Around("execution(* org.wt..*(..)) or execution(* com.sf..*(..))")
+	public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+		logger.info("groovyObject#invokeMethod");
+		return joinPoint.proceed();
+	}
+
+}
